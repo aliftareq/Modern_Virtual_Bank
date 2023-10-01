d051b764affec4215efa806112d0eca14299e462
alif <aliftareq@gmail.com>
2023-10-01 13:53:30 +0600

    hero section completed with Getstarted components

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,8 +1,19 @@
-
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
+import styles from "../style"
+import { arrowUp } from "../assets"
+const GetStarted = () =>  (
+  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+      <div className={`${styles.flexStart} flex-row`}>
+        <p className="font-poppins font-medium text-[18px] leading-[23px] text-gradient mr-2">
+          <span>Get</span>
+        </p>
+        <img src={arrowUp} alt="arrowsign" className="w-[23px] h-[23px] object-contain"/>
+      </div>
+      <p className="font-poppins font-medium text-[18px] leading-[23px] text-gradient">
+          <span>Started</span>
+        </p>
+    </div>
+  </div>
   )
-}
 
 export default GetStarted
