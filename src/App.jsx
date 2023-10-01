cf3af1b25803da5f7676bf77d8192c91e45ec55a
alif <aliftareq@gmail.com>
2023-10-01 10:13:51 +0600

    landing page layout setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 // import React from 'react'
+import styles from './style'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () =>  (
+  <div className="bg-primary w-full overflow-hidden">
+    {/* navbar */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+         Navbar
+      </div>
+    </div>
+    {/* hero section  */}
+    <div className={`bg-primary  ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+         hero
+      </div>
+    </div>
+    {/* hero section  */}
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+         hero
+      </div>
+    </div>
+  </div>
+)
 
 export default App
