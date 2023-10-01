31a49c3da606b6599c5963ea066451cb0e7d2e50
alif <aliftareq@gmail.com>
2023-10-01 11:31:53 +0600

    navbar setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-// import React from 'react'
 import styles from './style'
+import { Navbar, Hero,Stats ,Business ,Billing ,CardDeal ,Testimonials,
+Clients,CTA,Footer } from './components'
 
 const App = () =>  (
   <div className="bg-primary w-full overflow-hidden">
     {/* navbar */}
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-         Navbar
+         <Navbar/>
       </div>
     </div>
     {/* hero section  */}
     <div className={`bg-primary  ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-         hero
+         <Hero/>
       </div>
     </div>
-    {/* hero section  */}
+    {/* forepart section  */}
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-         hero
+         <Stats/> 
+         <Business/> 
+         <Billing/> 
+         <CardDeal/> 
+         <Testimonials/> 
+         <Clients />
+         <CTA/>
+         <Footer/>
       </div>
     </div>
   </div>
